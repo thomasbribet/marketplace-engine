36a119436ee6afeb6c83ec77607bce0410a410b2
Thomas <thomas.bribet@gmail.com>
2021-06-21 16:02:34 +0200

    heroku

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Menu />
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from './components/layout/Menu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Menu,
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './css/layout.css';
+@import './css/variables.css';
 </style>
---
+++ src/components/layout/Cards.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="cards">
+      <div class="card" v-for="attributCarte in carte" :key="attributCarte">
+        <img src="" alt="" />
+        <h4>{{ attributCarte }}</h4>
+      </div>
+    </div>
+</template>
+
+<script>
+import cardResume from '../admin/Admin-create-card.vue'
+
+export default {
+  data() {
+    return {
+      carte: cardResume,
+    }
+  },
+}
+</script>
+
+<style>
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  margin-bottom: 2%;
+}
+.card {
+  margin-top: 8%;
+  width: 371px;
+  height: 238px;
+  border-radius: 15px;
+  box-shadow: 2px 3px 10px 0px rgba(0, 0, 0, 0.4);
+  background-color: var(--whiteCard);
+  overflow: visible;
+}
+</style>
